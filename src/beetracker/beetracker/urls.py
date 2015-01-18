af5c927c96ad74cb023a11f2e67c9d7e09c89704
Dennis Schwertel <d.schwertel@s-v.de>
2015-01-18 23:08:50 +0100

    what?! broken

--- src/beetracker/beetracker/urls.py
+++ src/beetracker/beetracker/urls.py
@@ -3,14 +3,19 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from core.views import overview, detail, home
-import core
+#from core.views import overview, detail, home
+#from core import views #import core
+#from beetracker import core
+#import beetracker
 #from core.api import ActivityIndicationResource
 #from tastypie.api import Api
 from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 
+#print(beetracker)
+import core
 
+#print(core.utils)
 #v1_api = Api(api_name='v1')
 #v1_api.register(ActivityIndicationResource())
 
@@ -25,18 +30,21 @@ if 'rosetta' in settings.INSTALLED_APPS:
 
 urlpatterns += i18n_patterns('',
     #login required
-    url(r'^overview/', overview, name="overview"),
-    url(r'^detail/(?P<aid>\d+)', detail),
-
+    #url(r'^apiaries/list/', core.views.apiaries_list, name="apiaries_list"),
+    #url(r'^apiaries/detail/(?P<aid>\d+)', core.views.apiaires_detail, name="apiaries_detail"),
+    #url(r'^apiaries/new/', core.views.apiaires_new, name="apiaries_new"),
+    
     #not implemented
     #url(r'^heatmap/json/', heatmap_geojson),
     #url(r'^dataviz/', dataviz),
 
-    url(r'^accounts/register/', core.views.register, name="register"),
-    url(r'^accounts/login/', core.views.user_login, name="login"),
-    url(r'^accounts/logout/', core.views.user_logout, name="logout"),
+    #url(r'^about/', core.views.about, name="about"),
+
+    #url(r'^accounts/register/', core.views.register, name="register"),
+    #url(r'^accounts/login/', core.views.user_login, name="login"),
+    #url(r'^accounts/logout/', core.views.user_logout, name="logout"),
 
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^$', home, name='home'),
+    url(r'^$', core.views.home, name='home'),
 )
--- src/beetracker/core/views.py
+++ src/beetracker/core/views.py
@@ -1,93 +1,61 @@
-
-# Create your views here.
-
-from core.models import Apiary, Hive, WhatToDoSeason
-
-from core.osm import get_thing_by_tag, get_count_from_tag
-from core.owm import get_current_weather_temp, get_current_weather_humidity
-from django.db.models import Count, Avg
+#from core.models import Apiary, WhatToDoSeason
+#
+#from core.osm import get_thing_by_tag, get_count_from_tag
+#from core.owm import get_current_weather_temp, get_current_weather_humidity
+#
 from django.shortcuts import render
-from django.http import HttpResponse
-import json
-from decimal import Decimal
-from django.contrib.auth.decorators import login_required
-from django.template import RequestContext
-from django.shortcuts import render_to_response
-from django.contrib.auth import authenticate, login, logout#, hashers
-from django.http import HttpResponseRedirect
-"""
-@login_required
-def heatmap_geojson(request):
-    heatmap = Apiary.objects.values('latitude_approx', 'longitude_approx').annotate(Count("id")).order_by("-id__count")
-    ret = []
-    boxsize = Decimal(0.02)
-    for poly in heatmap:
-        heat = (float(poly["id__count"]) / float(heatmap[0]["id__count"]))
+#from django.contrib.auth.decorators import login_required
+#from django.template import RequestContext
+#from django.shortcuts import render_to_response
+#from django.contrib.auth import authenticate, login, logout  # hashers
+#from django.http import HttpResponseRedirect
+#
 
-        dic = {}
-        dic["type"] = "Feature"
-        dic["properties"] = {"heat": heat}
-        dic["geometry"] =  {
-            "type": "Polygon",
-            "coordinates": [[
-                [float(poly["longitude_approx"]),  float(poly["latitude_approx"])],
-                [float(poly["longitude_approx"]+boxsize),  float(poly["latitude_approx"])],
-                [float(poly["longitude_approx"]+boxsize),  float(poly["latitude_approx"]+boxsize)],
-                [float(poly["longitude_approx"]),  float(poly["latitude_approx"]+boxsize)],
-                [float(poly["longitude_approx"]),  float(poly["latitude_approx"]+boxsize)]
-            ]]
-        }
-        ret.append(dic)
-    return HttpResponse(json.dumps(ret), content_type="application/json")
-
-
-@login_required
-def dataviz(request):
-    return render(request, "dataviz.html", {
-        'chart_apiaries' : len(Apiary.objects.all()),
-        'chart_hives' : len(Hive.objects.all()),
-        'avg_lat'  : Apiary.objects.all().aggregate(Avg('latitude_approx'))['latitude_approx__avg'],
-        'avg_long' : Apiary.objects.all().aggregate(Avg('longitude_approx'))['longitude_approx__avg'],
-    
-        })
-"""
 
 def home(request):
     return render(request, "home.html", {})
 
+""""
 
 @login_required
-def overview(request):
-    return render(request, "overview.html", {'apiaries': Apiary.objects.all()})
+def apiaires_new(request):
+    return render(request, "apiaries/new.html", {})
 
 
 @login_required
-def detail(request, aid):
+def apiaires_list(request):
+    return render(request, "apiaries/list.html", {'apiaries': Apiary.objects.all()})
+
+
+@login_required
+def apiaries_detail(request, aid):
     apiary = Apiary.objects.get(id=aid)
     whattodo = WhatToDoSeason.objects.all()
 
-    natural_tree_species = get_thing_by_tag(apiary = apiary, tag_key="natural", tag_value="tree", thing=["species:de", "species", "name:botanical"])
-    natural_tree_type = get_thing_by_tag(apiary = apiary, tag_key="natural", tag_value="tree", thing=["type"])
-    number_trees = get_count_from_tag(apiary = apiary, tag_key="natural", tag_value="tree")
+    natural_tree_species = get_thing_by_tag(apiary=apiary, tag_key="natural", tag_value="tree", thing=["species:de", "species", "name:botanical"])
+    natural_tree_type = get_thing_by_tag(apiary=apiary, tag_key="natural", tag_value="tree", thing=["type"])
+    number_trees = get_count_from_tag(apiary=apiary, tag_key="natural", tag_value="tree")
     leisure_parks = get_thing_by_tag(apiary=apiary, tag_key="leisure", tag_value="park")
     landuse_vineyards = get_thing_by_tag(apiary=apiary, tag_key="landuse", tag_value="vineyard")
-    
     weather_temp = get_current_weather_temp(apiary=apiary)
     weather_humidity = get_current_weather_humidity(apiary=apiary)
 
-
-    return render(request, "detail.html", {
+    return render(request, "apiaries/detail.html", {
         'apiary': apiary,
         'apiaries': Apiary.objects.all(),
         'number_trees': number_trees,
-        'natural_tree_species': natural_tree_species, 
+        'natural_tree_species': natural_tree_species,
         'natural_tree_type': natural_tree_type,
-        'leisure_park_chart': leisure_parks, 
-        'landuse_vineyard_chart':landuse_vineyards,
-        'weather_temp' : weather_temp,
-        'weather_humidity' : weather_humidity,
-        'whattodo' : whattodo
+        'leisure_park_chart': leisure_parks,
+        'landuse_vineyard_chart': landuse_vineyards,
+        'weather_temp': weather_temp,
+        'weather_humidity': weather_humidity,
+        'whattodo': whattodo
         })
+
+
+#def about(request):
+#    return render(request, "about.html", {})
 
 
 def register(request):
@@ -99,6 +67,7 @@ def user_logout(request):
     logout(request)
     # Take the user back to the homepage.
     return HttpResponseRedirect("/")
+
 
 def user_login(request):
     context = RequestContext(request)
@@ -113,9 +82,45 @@ def user_login(request):
                 login(request, user=user)
                 return HttpResponseRedirect("/")
             else:
-                success = False 
+                success = False
         else:
-            success = False    
+            success = False
     return render_to_response('accounts/login.html', {
         'success': success,
         }, context)
+"""
+
+
+#@login_required
+#def heatmap_geojson(request):
+#    heatmap = Apiary.objects.values('latitude_approx', 'longitude_approx').annotate(Count("id")).order_by("-id__count")
+#    ret = []
+#    boxsize = Decimal(0.02)
+#    for poly in heatmap:
+#        heat = (float(poly["id__count"]) / float(heatmap[0]["id__count"]))
+#
+#        dic = {}
+#        dic["type"] = "Feature"
+#        dic["properties"] = {"heat": heat}
+#        dic["geometry"] =  {
+#            "type": "Polygon",
+#            "coordinates": [[
+#                [float(poly["longitude_approx"]),  float(poly["latitude_approx"])],
+#                [float(poly["longitude_approx"]+boxsize),  float(poly["latitude_approx"])],
+#                [float(poly["longitude_approx"]+boxsize),  float(poly["latitude_approx"]+boxsize)],
+#                [float(poly["longitude_approx"]),  float(poly["latitude_approx"]+boxsize)],
+#                [float(poly["longitude_approx"]),  float(poly["latitude_approx"]+boxsize)]
+#            ]]
+#        }
+#        ret.append(dic)
+#    return HttpResponse(json.dumps(ret), content_type="application/json")
+#
+#
+#@login_required
+#def dataviz(request):
+#    return render(request, "dataviz.html", {
+#        'chart_apiaries' : len(Apiary.objects.all()),
+#        'chart_hives' : len(Hive.objects.all()),
+#        'avg_lat'  : Apiary.objects.all().aggregate(Avg('latitude_approx'))['latitude_approx__avg'],
+#        'avg_long' : Apiary.objects.all().aggregate(Avg('longitude_approx'))['longitude_approx__avg'],
+#        })
